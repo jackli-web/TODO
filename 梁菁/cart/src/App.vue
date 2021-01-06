14f1c1af35a97f00ca941984f99764c5ed5f9d03
嘻嘻 <2205526657@qq.com>
2021-01-06 10:52:04 +0800

    第二次提交

---
+++ 梁菁/cart/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html,body,#app{
+    background-color: #f4f5f7;
+    width: 100vw;
+  }
+</style>
---
+++ 梁菁/cart/src/components/common/tab.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="bottom">
+          <ul>
+            <li v-for="(item,index) in tarbar" :key="index" @click="jumpTo(index)">
+              <span :class="item"></span>
+            </li>
+          </ul>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    tarbar: ['iconfont icon-zhuye', 'iconfont icon-sousuo','iconfont icon-aixin','iconfont icon-yonghu']
+    }
+  },
+  methods: {
+   jumpTo(index) {
+      if(index == 3) {
+        this.$router.push(
+          {
+            name: 'use'
+          }
+        )
+      }
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+ .bottom {
+    position: fixed;
+    bottom: 0;
+    width: 100vw;
+    height: 22.1333vw;
+    filter: drop-shadow(0px -1px 0px rgba(0, 0, 0, 0.3));
+    background-color: #000;
+    ul{
+      display: flex;
+      padding: 0;
+      margin: 0;
+      height: calc( 22.1333vw - 9.0667vw);
+      background-color: #f9f9f9;
+    }
+    li {
+      position: relative;
+      flex: 1;
+      height: 100%;
+      list-style: none;
+      float: left;
+      margin: 0!important;
+      span {
+        position: absolute;
+        left: 10.6667vw;
+        top: 5.0667vw;
+        display: block;
+        width: 5.8667vw;
+        height: 5.8667vw;
+        font-size: 5.8667vw;
+      }
+    }
+  }
+</style>
