b960cec5a9a0a780e8b7eb44649ec6ca4c68e189
huxiaodong <319203890@qq.com>
2021-01-06 10:47:55 +0800

    晓冬

---
+++ 胡晓冬/vuex_demo/src/components/books.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="book" @click="xiangqing(bookData)">
+    <img :src="bookData.imgUrl" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+name:'books',
+props:['bookData'],
+methods:{
+  xiangqing(item){
+    this.$router.push({name:'products',query:item})
+  }
+}
+}
+</script>
+<style lang="less" scoped>
+  .book{
+    width: 26.6667vw;
+    height: 37.6vw;
+    background: antiquewhite;
+    margin-bottom: 9.8667vw;
+    img{
+      width: 26.6667vw;
+      height: 37.6vw;
+    }
+  }
+</style>
